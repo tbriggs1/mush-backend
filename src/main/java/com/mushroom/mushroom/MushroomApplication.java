32741db65b47737ccaa8b8c5063411891a622fa6
tbriggs1 <tombriggs1993@outlook.com>
2022-09-07 21:40:07 +0100

    first commit

---
+++ src/main/java/com/mushroom/mushroom/MushroomApplication.java
@@ -0,0 +1,13 @@
+package com.mushroom.mushroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushroomApplication.class, args);
+	}
+
+}
